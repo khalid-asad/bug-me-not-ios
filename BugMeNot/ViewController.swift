c54608cb49f83aa253caf545d39c1044d617b3b4
Khalid Asad <khalidasad93@gmail.com>
2021-04-03 20:28:29 -0400

    Created project.

---
+++ BugMeNot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BugMeNot
+//
+//  Created by Khalid Asad on 4/3/21.
+//  Copyright © 2021 Khalid Asad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
